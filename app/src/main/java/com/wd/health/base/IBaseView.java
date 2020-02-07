5b640b0505aa924ad0a3d4bd9fa08d85d60235cf
jcx <1846439119@qq.com>
2020-02-07 17:47:07 +0800

    好的

--- app/src/main/java/com/wd/health/base/IBaseView.java
+++ app/src/main/java/com/wd/health/base/IBaseView.java
@@ -2,11 +2,13 @@ package com.wd.health.base;
 
 import android.content.Context;
 
+/**
+ *@describe(描述)：IBaseView
+ *@data（日期）: 2020/2/7
+ *@time（时间）: 17:46
+ *@author（作者）: Jiaochengxiang
+ **/
 public interface IBaseView {
     Context context();
-
-
-    
-
 
 }
