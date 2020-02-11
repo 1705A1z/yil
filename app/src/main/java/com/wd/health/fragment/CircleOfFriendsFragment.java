52849f9d64afa173e44b744f1f7e490d8760e593
1705 <1705@163.com>
2020-02-11 21:49:16 +0800

    搜索框（有问题）

--- app/src/main/java/com/wd/health/fragment/CircleOfFriendsFragment.java
+++ app/src/main/java/com/wd/health/fragment/CircleOfFriendsFragment.java
@@ -1,12 +1,20 @@
 package com.wd.health.fragment;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.wd.health.R;
+import com.wd.health.aaa.MySearchHand;
 import com.wd.health.adapter.Disease.DiseaseAdapter;
 import com.wd.health.adapter.Disease.DiseaseCategoryAdapter;
 import com.wd.health.base.BaseFragment;
@@ -16,6 +24,9 @@ import com.wd.health.bean.DiseaseCategoryBean;
 import com.wd.health.presenter.DiseaseCategoryPresenter;
 
 import java.util.List;
+
+import butterknife.ButterKnife;
+import io.reactivex.annotations.Nullable;
 
 public class CircleOfFriendsFragment extends BaseFragment<DiseaseCategoryPresenter> implements DiseaseCategoryContract.IView {
     private static final String TAG = "CircleOfFriendsFragment";
@@ -99,4 +110,75 @@ public class CircleOfFriendsFragment extends BaseFragment<DiseaseCategoryPresent
 
     }
 
+    public static class MySearchHand extends LinearLayout {
+        //    @BindView(R2.id.srarch_back)
+        ImageView imgview;
+        //    @BindView(R2.id.search_edtext)
+        EditText tv_shou;
+        //    @BindView(R2.id.search_sosuo)
+        ImageView rlv2;
+
+        /**
+         * 自定义搜索框
+         */
+        public MySearchHand(Context context) {
+            this(context, null);
+        }
+
+        public MySearchHand(Context context, @Nullable AttributeSet attrs) {
+            this(context, attrs, -1);
+        }
+
+        public MySearchHand(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+            super(context, attrs, defStyleAttr);
+            init(context);
+        }
+
+        /**
+         * 自定义搜索框
+         * 加载布局
+         * 按钮
+         * 获取输入的值 接口回调
+         */
+        private void init(Context context) {
+//        View inflate = LayoutInflater.from(context).inflate(R.layout.search_item, this, true);
+            View inflate = LayoutInflater.from(context).inflate(R.layout.circle_fragment, this, true);
+            ButterKnife.bind(this, inflate);
+       /* ImageView srarch_back = inflate.findViewById(R.id.srarch_back);
+        ImageView srarch_Iv = inflate.findViewById(R.id.search_sosuo);
+        final EditText srarch_edText = inflate.findViewById(R.id.search_edtext);*/
+            /** back
+             *  sosuo
+             * */
+            imgview.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mOnBackCilckListener.onBackClick();
+                }
+            });
+            rlv2.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //输入内容
+                    String srarch = tv_shou.getText().toString().trim();
+                    mOnBackCilckListener.onTextListener(srarch);
+                }
+            });
+        }
+
+        /**
+         * 接口回调事件
+         */
+        public interface OnBackCilckListener {
+            void onBackClick();
+
+            void onTextListener(String text);
+        }
+
+        com.wd.health.aaa.MySearchHand.OnBackCilckListener mOnBackCilckListener;
+
+        public void setOnBackCilckListener(com.wd.health.aaa.MySearchHand.OnBackCilckListener onBackCilckListener) {
+            mOnBackCilckListener = onBackCilckListener;
+        }
+    }
 }
