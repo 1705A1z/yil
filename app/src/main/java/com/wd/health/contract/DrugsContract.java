bbd354d81655f3364be3c1a1816f72bbb8b4432e
jcx <1846439119@qq.com>
2020-02-25 20:08:35 +0800

    药2

--- app/src/main/java/com/wd/health/contract/DrugsContract.java
+++ app/src/main/java/com/wd/health/contract/DrugsContract.java
@@ -1,4 +1,27 @@
 package com.wd.health.contract;
 
-public class DrugsContract {
+import com.wd.health.base.IBaseView;
+import com.wd.health.bean.DrugsBean;
+
+public interface DrugsContract {
+    interface IView extends IBaseView {
+
+        //药
+        void DrugsSuccess(DrugsBean drugsBean);
+        void DrugsErorr(String mag);
+
+    }
+    interface IModel{
+
+        void getDrugs(DrugsCallBack drugsCallBack);
+
+        interface DrugsCallBack{
+            void DrugsSuccess(DrugsBean drugsBean);
+            void DrugsErorr(String mag);
+        }
+
+    }
+    interface IPresenter{
+        void getDrugs();
+    }
 }
